8bed8b4ed4fdf2f2f3577b30a6dea883354d16c4
Gary Yin <ygchina@hotmail.com>
2020-07-19 10:13:46 -0600

    change domain name

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(dev-srv.centralus.cloudapp.azure.com)
+domains=(whatsnow.ca)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="ygchina@hotmail.com" # Adding a valid address is strongly recommended
